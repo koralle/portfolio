a65e5c35772ae8f4be12df23dddcce0ff25479ba
koralle <mkhd7930@gmail.com>
2025-06-01 23:44:00 +0900

    feat: add custom breakpoints to Panda CSS configuration

--- panda.config.ts
+++ panda.config.ts
@@ -26,7 +26,16 @@ export default defineConfig({
         },
       },
     },
-    extend: {},
+    extend: {
+      breakpoints: {
+        xs: '480px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
   },
 
   globalCss: globalStyles,
